59330e3feeee4e386a9bad993c07e90ea8b30675
Angelina Anokhina <Angelina.anokhina9@gmail.com>
2025-06-13 00:22:50 +0700

    f

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import PhotoSession from './components/PhotoSession/PhotoSession';
 import './Fonts.module.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AboutUs from './components/AboutUs/AboutUs';
 import Services from './components/Services/Services';
 import ContactForm from './components/ContactForm/ContactForm';
@@ -17,12 +17,12 @@ const App = () => {
         <Header />
         <div className='site-wrapper-content'>
           <Routes>
-            <Route path="/PhotoSession" element={<PhotoSession />} />
-            <Route path="/AboutUs" element={<AboutUs />} />
-            <Route path="/Services" element={<Services />} />
-            <Route path="/Portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<ContactForm />} />
             <Route path="/" element={<PhotoSession />} />
+            <Route path="/photosession" element={<PhotoSession />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<ContactForm />} />
           </Routes>
         </div>
         <Footer />
