98059c9a920f1e982417552dd5fae59368682094
Angelina Anokhina <Angelina.anokhina9@gmail.com>
2025-06-23 18:18:46 +0700

    ..

--- src/components/PhotoSession/Gallery/GallerySection.jsx
+++ src/components/PhotoSession/Gallery/GallerySection.jsx
@@ -17,7 +17,7 @@ const GallerySection = () => {
         <img src={`${process.env.PUBLIC_URL}/img/portrait1.svg`} alt="Пример работы 1" className={s.img1} />
         <img src={`${process.env.PUBLIC_URL}/img/image5.svg`} alt="Пример работы 2" className={s.img2} />
         <img src={`${process.env.PUBLIC_URL}/img/image6.svg`} alt="Пример работы 3" className={s.img3} />
-        <img src={`${process.env.PUBLIC_URL}/img/image7.svg`} alt="Пример работы 4" className={s.img4} />
+        <img src={`${process.env.PUBLIC_URL}/img/image7.svg`} alt="Пример работы 4" className={s.img4} />тт
 
 
         <div className={s.buttonWrapper}>
